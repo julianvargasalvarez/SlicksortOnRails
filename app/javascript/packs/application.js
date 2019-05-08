4d76fda8e0d839681ff3a0ef5b5d8f230ce870dd
Julian Vargas <julianvargasalvarez@gmail.com>
2019-05-08 07:24:39 -0500

    setup slicksort code sample

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,82 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+/* eslint no-console: 0 */
+// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
+// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
+// to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+// All it does is render <div>Hello Vue</div> at the bottom of the page.
+
+// import Vue from 'vue'
+// import App from '../app.vue'
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const el = document.body.appendChild(document.createElement('hello'))
+//   const app = new Vue({
+//     el,
+//     render: h => h(App)
+//   })
+// 
+//   console.log(app)
+// })
+
+
+// The above code uses Vue without the compiler, which means you cannot
+// use Vue to target elements in your existing html templates. You would
+// need to always use single file components.
+// To be able to target elements in your existing html/erb templates,
+// comment out the above code and uncomment the below
+// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
+// Then add this markup to your html template:
 //
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+// <div id='hello'>
+//   {{message}}
+//   <app></app>
+// </div>
 
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import Vue from 'vue/dist/vue.esm'
+import { ContainerMixin, ElementMixin } from 'vue-slicksort';
 
-console.log('Hello World from Webpacker')
+const SortableList = {
+  mixins: [ContainerMixin],
+  template: `
+    <ul class="list">
+      <slot />
+    </ul>
+  `
+};
+
+const SortableItem = {
+  mixins: [ElementMixin],
+  props: ['item'],
+  template: `
+    <li class="list-item">{{item}}</li>
+  `
+};
+
+const ExampleVue = {
+  name: 'Example',
+  template: `
+    <div class="root">
+      <SortableList lockAxis="y" v-model="items">
+        <SortableItem v-for="(item, index) in items" :index="index" :key="index" :item="item"/>
+      </SortableList>
+    </div>
+  `,
+  components: {
+    SortableItem,
+    SortableList
+  },
+  data() {
+    return {
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8']
+    };
+  }
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#main',
+    render: h => h(ExampleVue)
+  });
+})
